6dcaebad5da136a42542b3ef7f2f31311df61462
Adam Tran <adam@tdtran.cz>
2025-11-28 21:23:18 +0900

    🐛 fix uninit players array

--- lib/game/config.ts
+++ lib/game/config.ts
@@ -9,7 +9,11 @@ export const MIN_IMPOSTORS = 1;
 export const INITIAL_GAME_CONFIG: GameConfig = {
   totalPlayers: MIN_PLAYERS,
   impostorCount: MIN_IMPOSTORS,
-  players: [],
+  players: Array.from({ length: MIN_PLAYERS }, (_, index) => ({
+    id: index + 1,
+    name: "",
+    isImpostor: false,
+  })),
   currentPlayerIdx: 0,
   gameType: "paper",
   namesEnabled: true,
