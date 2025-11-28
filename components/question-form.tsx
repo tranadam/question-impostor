775f640cb25a634b5291f23ecbc0e16ddda691ae
Adam Tran <adam@tdtran.cz>
2025-11-28 18:41:51 +0900

    ✨ add edit settings button

--- components/question-form.tsx
+++ components/question-form.tsx
@@ -1,32 +1,20 @@
-"use client";
-
 import { GameConfig } from "@/types/game";
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
   TypographyH2,
   TypographyMuted,
   TypographySmall,
 } from "@/components/ui/typography";
-import { Dices, Sparkles } from "lucide-react";
-import { toast } from "sonner";
 
 export default function QuestionForm({
   config,
   setMainQuestion,
   setImpostorQuestion,
-  onNext,
 }: {
   config: GameConfig;
   setMainQuestion: (mainQuestion: string) => void;
   setImpostorQuestion: (impostorQuestion: string) => void;
-  onNext: () => void;
 }) {
-  const handleAIInspiration = () => {
-    toast.warning("AI inspiration coming soon! (surely)");
-    return;
-  };
-
   return (
     <section className="flex flex-col gap-8">
       <TypographyH2>Questions</TypographyH2>
@@ -52,16 +40,6 @@ export default function QuestionForm({
           {config.impostorCount} impostor
           {config.impostorCount > 1 ? "s" : ""} see this question
         </TypographyMuted>
-      </div>
-      <div className="flex flex-col gap-2">
-        <Button onClick={handleAIInspiration} variant="outline">
-          <Sparkles />
-          get inspired by ai
-        </Button>
-        <Button onClick={onNext}>
-          <Dices />
-          let&apos;s play
-        </Button>
       </div>
     </section>
   );
--- components/screens/question-form-screen.tsx
+++ components/screens/question-form-screen.tsx
@@ -1,6 +1,10 @@
 import { GameConfig } from "@/types/game";
 import QuestionForm from "@/components/question-form";
 import { generateRandomIntFromRange } from "@/lib/random";
+import { Button } from "@/components/ui/button";
+import { Dices, Sparkles } from "lucide-react";
+import { toast } from "sonner";
+import Link from "next/link";
 
 export default function QuestionFormScreen({
   config,
@@ -28,6 +32,11 @@ export default function QuestionFormScreen({
     updateConfig({ currentPlayerIdx: 0 });
     onNext();
   };
+  const handleAIInspiration = () => {
+    toast.warning("AI inspiration coming soon! (surely)");
+    return;
+  };
+
   return (
     <main className="mx-auto mb-16 max-w-2xl px-4">
       <QuestionForm
@@ -36,8 +45,25 @@ export default function QuestionFormScreen({
         setImpostorQuestion={(q: string) =>
           updateConfig({ impostorQuestion: q })
         }
-        onNext={handleNext}
       />
+      <div className="mt-8 flex flex-col gap-2">
+        <Button
+          disabled={
+            config.mainQuestion === "" || config.impostorQuestion === ""
+          }
+          onClick={handleNext}
+        >
+          <Dices />
+          let&apos;s play
+        </Button>
+        <Button onClick={handleAIInspiration} variant="outline">
+          <Sparkles />
+          get inspired by ai
+        </Button>
+        <Button variant="ghost" asChild>
+          <Link href="/">change settings</Link>
+        </Button>
+      </div>
     </main>
   );
 }
