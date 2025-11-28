3a23d5159b4940134c37714cb3c1b6ffcea6c3cc
Adam Tran <adam@tdtran.cz>
2025-11-28 21:29:59 +0900

    ✨ implement PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+      </head>
       <body className={poppins.className}>
         <Header />
         {children}
--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,11 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
 const nextConfig: NextConfig = {
   turbopack: {
@@ -12,4 +19,4 @@ const nextConfig: NextConfig = {
   output: "standalone",
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
