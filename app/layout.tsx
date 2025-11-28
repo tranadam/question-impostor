a643a351c82e44d17e849dceb62b290bfdfc3b43
Adam Tran <adam@tdtran.cz>
2025-11-28 19:07:02 +0900

    🍱 add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="Impostor" />
+      </head>
       <body className={poppins.className}>
         <Header />
         {children}
